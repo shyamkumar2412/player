60c41bbb1de233587a8243511bfa9ab1404d600a
shyamkumar2412 <51222787+shyamkumar2412@users.noreply.github.com>
2019-07-01 12:53:25 +0530

    Update 79.py

--- 79.py
+++ 79.py
@@ -1,9 +1,12 @@
 a,b=list(map(int,input().split()))
 c=a*b
-for i in range(1,c):
-    b=i*i
-    if b==c:
-        print('yes')
-        break
+if c!=0:
+    for i in range(0,c):
+        b=i*i
+        if b==c:
+            print('yes')
+            break
+    else:
+        print('no')
 else:
-    print('no')
+    print('yes')
