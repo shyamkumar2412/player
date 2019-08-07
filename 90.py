cdaae473be77cd9c85a349c011823df5ec70de9e
shyamkumar2412 <51222787+shyamkumar2412@users.noreply.github.com>
2019-08-07 20:52:58 +0530

    Update 90.py

--- 90.py
+++ 90.py
@@ -1,11 +1,11 @@
-n,m=list(map(int,input().split()))
+a,b=list(map(int,input().split()))
 l=[]
 s=1
 x=1
-for i in range(m):
-    s=s*n
-    n=n-1
-for i in range(1,m+1):
+for i in range(b):
+    s=s*a
+    a=a-1
+for i in range(1,b+1):
     x=x*i
 print(s//x)
     
